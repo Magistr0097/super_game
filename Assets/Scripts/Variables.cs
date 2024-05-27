b06c17109106c42c46e49c47d1d3df189d7c206e
Magistr0097 <44272119+Magistr0097@users.noreply.github.com>
2024-05-28 00:31:49 +0500

    Added colliders

--- Assets/Scripts/Variables.cs
+++ Assets/Scripts/Variables.cs
@@ -7,4 +7,8 @@ public class Variables : MonoBehaviour
     public bool MoveTutorialComplete = false;
     public int ForestStage = 0;
     public Dictionary<string, string[]> linesDict = new Dictionary<string, string[]>();
+    public Transform[] RespawnPoints =
+    {
+        
+    };
 }
